85a568845c112c20b09f1b15bc7c047f88830f18
Rohan19005742 <60757115+Rohan19005742@users.noreply.github.com>
2022-10-13 16:35:20 +0100

    13/10/2022
    Completed task

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    count = 2
+    while (len(list) != number_of_primes):
+        if (isPrime(count)):
+            list.append(count)
+        count+=1
     return list
+
+def isPrime(number):
+    for x in range(int(number/2)+1):
+        if (x == 0 or x == 1):
+            continue
+        if (number % x == 0):
+            return False
+    return True
